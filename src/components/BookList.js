d852939ee4f41c7f5d61b4aea9961b7e64d146e4
Matt Baxter <mbx33@hotmail.com>
2022-08-01 11:05:34 -0600

    initial

---
+++ src/components/BookList.js
@@ -0,0 +1,18 @@
+//firebase setting up a delete function
+
+export default function BookList({ books }) {
+
+  const handleClick = async (id) => {
+    console.log(id)
+  }
+
+  return (
+    <div className="book-list">
+      <ul>
+        {books.map(book => (
+          <li key={book.id} onClick={() => handleClick(book.id)}>{book.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
